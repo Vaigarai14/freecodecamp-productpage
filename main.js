03e50adbe21129fdc37b0f88c2b9f8f3398d4100
Vaigarai14 <vaigaraithennarasu144@gmail.com>
2024-01-03 16:41:59 +0530

    modified for moblie view

--- main.js
+++ main.js
@@ -1,19 +1,31 @@
-if (typeof document !== 'undefined') {
-    var button = document.getElementById('btn');
-    let input = document.getElementById('Accno');
-    var resultvalue = document.getElementById('accvalue');
+// // if (typeof document !== 'undefined') {
+// //     var button = document.getElementById('btn');
+// //     let input = document.getElementById('Accno');
+// //     var resultvalue = document.getElementById('accvalue');
 
-    function createElement() {
-        if (input.value.length > 0) {
-            let create = document.createElement('p');
-            let inputText = document.createTextNode(input.value);
-            create.appendChild(inputText);
-            resultvalue.appendChild(create);
-            input.value = ""
-            return resultvalue.innerText
-        }
-    }
+// //     function createElement() {
+// //         if (input.value.length > 0) {
+// //             let create = document.createElement('p');
+// //             let inputText = document.createTextNode(input.value);
+// //             create.appendChild(inputText);
+// //             resultvalue.appendChild(create);
+// //             input.value = ""
+// //             return resultvalue.innerText
+// //         }
+// //     }
 
-    button.addEventListener('click', createElement);
+// //     button.addEventListener('click', createElement);
 
-}
+// // }
+
+
+// const navbar = document.querySelector('#nav-bar');
+// let top = navbar.offsetTop;
+// function stickynavbar() {
+//     if (window.scrollY >= top) {
+//         navbar.classList.add('sticky');
+//     } else {
+//         navbar.classList.remove('sticky');
+//     }
+// }
+// window.addEventListener('scroll', stickynavbar);
